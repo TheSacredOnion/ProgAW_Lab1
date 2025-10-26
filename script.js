bb89d7118c729e74df1ca80119959a093979918e
Adam Kamiński <adam29kaminski@gmail.com>
2025-10-26 13:25:01 +0100

    Zadanie 1 commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-(function () {
-  //TODO
-})();
+const button = document.getElementById("ex1_button");
+button.addEventListener("click", function() {
+  let numbers = "";
+  for (let i = 0; i < 10; i++) {
+    numbers += i + ", ";
+  }
+  document.getElementById("ex1_content").innerText = numbers.slice(0, -2);
+});
