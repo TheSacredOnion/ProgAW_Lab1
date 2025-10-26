8fb20c1aee54e1005fc743e9c29c5fad60c2a3ee
Adam Kamiński <adam29kaminski@gmail.com>
2025-10-26 13:40:14 +0100

    Zadanie 2 Commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Zadanie 1
 const button = document.getElementById("ex1_button");
 button.addEventListener("click", function() {
   let numbers = "";
@@ -6,3 +7,30 @@ button.addEventListener("click", function() {
   }
   document.getElementById("ex1_content").innerText = numbers.slice(0, -2);
 });
+
+// Zadanie 2
+const inputNumber = document.getElementById("ex2_text");
+const contentDiv = document.getElementById("ex2_content");
+
+function validatePhoneNumber() {
+  const phoneNumber = inputNumber.value;
+  let errors = [];
+
+  if (phoneNumber.length !== 9) {
+    errors.push("Długość numeru telefonu musi wynosić 9 cyfr.");
+  }
+  if (/[a-zA-Z]/.test(phoneNumber)) {
+    errors.push("Numer telefonu nie może zawierać liter.");
+  }
+  if (/[!@#$%^&*(),.?":{}|<>]/.test(phoneNumber)) {
+    errors.push("Numer telefonu nie może mieć znaków specjalnych.");
+  }
+
+  if (errors.length === 0) {
+    contentDiv.textContent = "Numer telefonu jest poprawny.";
+  } else {
+    contentDiv.innerHTML = errors.join("<br>");
+  }
+}
+
+inputNumber.addEventListener("input", validatePhoneNumber);
